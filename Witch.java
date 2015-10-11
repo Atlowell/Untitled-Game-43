4e676ec739c324d74ffefde60d8f7b8a39db2a16
burgosd <burgosd@purdue.edu>
2015-10-10 23:37:53 -0400

    Create Witch.java

---
+++ Witch.java
@@ -0,0 +1,13 @@
+import java.util.Hashtable;
+
+public class Witch extends Monster {
+
+    public Witch() {
+        name = "Witch";
+        attack = 3;
+        health = 12;
+        loottable = new Hashtable<Integer, Loot>();
+        loottable.put(56, new Loot("Potion"));
+        loottable.put(99, new Loot("Witch's Hat"));
+    }
+}
