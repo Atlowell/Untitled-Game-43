22ab5011d5edfdced560dae9710bdaa824fa6f65
burgosd <burgosd@purdue.edu>
2015-10-10 23:38:59 -0400

    Create WildDog.java

---
+++ WildDog.java
@@ -0,0 +1,12 @@
+import java.util.Hashtable;
+
+public class WildDog extends Monster {
+
+    public WildDog() {
+        name = "Wild Dog";
+        attack = 1;
+        health = 4;
+        loottable = new Hashtable<Integer, Loot>();
+        loottable.put(99, new Loot("Dog Bones"));
+    }
+}
